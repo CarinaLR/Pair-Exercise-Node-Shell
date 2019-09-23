5b8419820f34feb434d3e6594416fe2caf61768f
SimonG <saguti15@g.holycross.edu>
2019-09-23 13:35:49 -0400

    final push

--- pwd.js
+++ pwd.js
@@ -2,7 +2,7 @@
 
 module.exports = function pwd() {
     process.stdout.write('prompt > ');
-const pwd = process.stdin.on('data', () => {
+    const pwd = process.stdin.on('data', () => {
     
     process.stdout.write(process.cwd());
     process.stdout.write('\nprompt > ');
