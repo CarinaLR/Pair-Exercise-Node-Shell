2950d8c8764adaf5418788a7e3694e5c9252f663
SimonG <saguti15@g.holycross.edu>
2019-09-23 13:32:56 -0400

    started node-shell

---
+++ pwd.js
@@ -0,0 +1,10 @@
+
+
+module.exports = function pwd() {
+    process.stdout.write('prompt > ');
+const pwd = process.stdin.on('data', () => {
+    
+    process.stdout.write(process.cwd());
+    process.stdout.write('\nprompt > ');
+});
+}
